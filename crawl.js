4a7776d59ae8026ca60d37f7f9f0afeaaeee1702
data-roshi <dataroshi@gmail.com>
2024-07-06 17:57:04 +0200

    add get url

--- crawl.js
+++ crawl.js
@@ -1,3 +1,27 @@
+const { JSDOM } = require('jsdom');
+
+const getURLsFromHTML = (htmlBody, baseURL) => {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        const url = linkElement.href;
+        if (!url) {
+            continue;
+        }
+
+        const relativeURL = linkElement.href.slice(0, 1) === '/';
+        const fullURL = relativeURL ? `${baseURL}${linkElement.href}` : linkElement.href;
+        try {
+            const urlObj = new URL(fullURL);
+            urls.push(urlObj.href)
+        } catch (err) {
+            console.log(`error ${relativeURL ? 'relative' : 'absolute'} url: ${err.message}`);
+        }
+    }
+    return urls;
+}
+
 const normalizeURL = (url) => {
     const urlObj = new URL(url);
     let hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +32,6 @@ const normalizeURL = (url) => {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl');
+const { normalizeURL, getURLsFromHTML } = require('./crawl');
 const { expect, test} = require('@jest/globals');
 
 test('normalizeURL strip protocol', () => {
@@ -37,6 +37,98 @@ test('normalizeURL strip http', () => {
     const expected = 'blog.boot.dev/path';
     // WHEN
     const result = normalizeURL(input);
+    // THEN
+    expect(result).toEqual(expected);
+});
+
+test('getURLsFromHTML absolute', () => {
+    // GIVEN
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">
+                boot.dev blog 
+            </a>
+        </body> 
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev"
+    const expected = ["https://blog.boot.dev/path/"];
+    // WHEN
+    const result = getURLsFromHTML(inputHTMLbody, inputBaseURL);
+    // THEN
+    expect(result).toEqual(expected);
+});
+
+test('getURLsFromHTML empty href', () => {
+    // GIVEN
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="">
+                boot.dev blog 
+            </a>
+        </body> 
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev"
+    // WHEN
+    const result = getURLsFromHTML(inputHTMLbody, inputBaseURL);
+    // THEN
+    expect(result).toEqual([]);
+});
+
+test('getURLsFromHTML relative', () => {
+    // GIVEN
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="/path/">
+                boot.dev blog 
+            </a>
+        </body> 
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev"
+    const expected = ["https://blog.boot.dev/path/"];
+    // WHEN
+    const result = getURLsFromHTML(inputHTMLbody, inputBaseURL);
+    // THEN
+    expect(result).toEqual(expected);
+});
+
+test('getURLsFromHTML invalid url', () => {
+    // GIVEN
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="invalid-url">
+                invalid url
+            </a>
+        </body> 
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev"
+    const expected = ["https://blog.boot.dev/path/"];
+    // WHEN
+    const result = getURLsFromHTML(inputHTMLbody, inputBaseURL);
+    // THEN
+    expect(result).toEqual([]);
+});
+
+test('getURLsFromHTML absolute and relative', () => {
+    // GIVEN
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/">
+                boot.dev blog 1
+            </a>
+            <a href="/path2/">
+                boot.dev blog 2
+            </a>
+        </body> 
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev"
+    const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"];
+    // WHEN
+    const result = getURLsFromHTML(inputHTMLbody, inputBaseURL);
     // THEN
     expect(result).toEqual(expected);
 });
